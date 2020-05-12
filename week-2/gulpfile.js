433499913ebc3ab0a555de9f7d2ec9626b164e86
B612268X <64888010+B6122683@users.noreply.github.com>
2020-05-12 16:00:48 +0700

    Upload
    Upload

---
+++ week-2/gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest, watch, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const minifyCSS = require("gulp-csso");
+function css() {
+    return src("scss/*.scss")
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(dest("css"));
+}
+
+function watchFiles() {
+    watch("./scss/**/*", css);
+}
+
+exports.css = css;
+exports.watch = watch;
+exports.default = parallel(css, watchFiles);
